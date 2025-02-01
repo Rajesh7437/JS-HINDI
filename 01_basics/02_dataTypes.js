d6eec54e20d4e631919c437840f48c2bf4dfdc75
Rajesh kumar Das <160344892+Rajesh7437@users.noreply.github.com>
2025-02-01 04:30:11 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,36 @@
+ "Use strict"; //treat all js code as a neew version
+ 
+//  alert( 5-2 ) //we are using node js, not browser
+
+console.log( 3 + 
+     3); //code readability should be high
+
+     console.log( "Rajesh");
+
+     let name = "rajesh"
+     let age = 22
+     let isLoggedIn = true
+     let state;
+
+
+    // number => 2 to power 53
+    // bigint
+    // string => ""
+    // boolean => True/False
+    // null => standalone value
+    // undefined => 
+    // symbol => unique
+
+
+
+    // object
+
+
+    console.log(typeof null); // object
+    console.log(typeof undefined); // undefined
+    
+
+    
+    
+     
+
