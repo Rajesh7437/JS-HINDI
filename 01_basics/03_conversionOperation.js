9edf8c1c0b1fb64bff93a81912fb5f79d9d5ad01
Rajesh kumar Das <160344892+Rajesh7437@users.noreply.github.com>
2025-02-11 05:38:44 +0000

    ConversionOperation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,38 @@
+let score = "23abs"
+
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+let valueInNumber = Number(score)
+//console.log( valueInNumber);
+
+
+// "23" => 23
+// "23" => Nan (Not a number)
+// true = 1;  False => 0;
+
+
+//let isLoggedIn = "00"
+//let booleanisLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanisLoggedIn);
+
+
+// 1 => true;  0 => false
+// "" => false;
+// "rajesh" => true;
+
+
+let someNumber= 23
+let stringNumber = String(someNumber)
+
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+
+
+
+
+
+
