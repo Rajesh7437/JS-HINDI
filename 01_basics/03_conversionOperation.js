2f1b1fa3ec2e142251a5c7af0183c57427f56287
Rajesh kumar Das <160344892+Rajesh7437@users.noreply.github.com>
2025-02-11 14:31:58 +0000

    conversionOperation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -12,9 +12,8 @@ let valueInNumber = Number(score)
 // "23" => Nan (Not a number)
 // true = 1;  False => 0;
 
-
-//let isLoggedIn = "00"
-//let booleanisLoggedIn = Boolean(isLoggedIn)
+let isLoggedIn = "00"
+let booleanisLoggedIn = Boolean(isLoggedIn)
 //console.log(booleanisLoggedIn);
 
 
@@ -26,8 +25,66 @@ let valueInNumber = Number(score)
 let someNumber= 23
 let stringNumber = String(someNumber)
 
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+
+//************************  Operations  ****************************//
+
+let value = 5
+let negValue = -value
+// console.log(negValue);
+
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+
+
+str1 = "Hello"
+str2 = " Rajesh"
+str3 = str1 + str2
+// console.log(str3);
+
+
+// console.log(1 + "2");
+// console.log("1" + 2);
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+
+// console.log(3 + (4 * 5) % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2
+//console.log(num1);
+
+
+let gameCounter = 200
+++gameCounter;
+console.log(gameCounter);
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
