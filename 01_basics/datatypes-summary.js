2c3690319a2fd17cc332d29eb73ad6a1ec80a9e4
Rajesh kumar Das <160344892+Rajesh7437@users.noreply.github.com>
2025-02-15 06:11:48 +0000

    datatypes-summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,42 @@
+// #  Primitive
+
+// 7 types : String, Number, boolean, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLogggedIn = true
+const outsidetemp = null
+let userEmail;
+
+const id = Symbol('2131')
+const anotherId = Symbol('2131')
+// console.log( id === anotherId);
+
+const bigNumber = 6456464354324342345n
+// console.log(bigNumber);
+
+
+
+
+
+
+
+// Reference (Non primitive)
+
+// Array, Objects, Function
+
+const heros = ["shaktiman, superman, leo"];
+let myObj = {
+
+    nmae: "raju",
+    age: 22
+}
+
+
+const myfunction = function(){
+    console.log("Hello world");
+    
+}
+
+console.log(typeof myfunction);
